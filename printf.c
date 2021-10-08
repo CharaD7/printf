161298f4db42dd4dbb5762bc8f450753272b3b6b
CharaD7 <jijakahn6@gmail.com>
2021-10-08 21:34:30 +0000

    Got rid of linting errors

--- printf.c
+++ printf.c
@@ -34,7 +34,9 @@ int printTypeChecker(char car, va_list arg)
 }
 
 /**
- * printf - Our custom printf function based on C's
+ * _printf - Our custom printf function based on C's
+ *
+ * Desctiption: A mimic of C's printf function
  *
  * @format : Character stream to be used
  *
@@ -46,54 +48,36 @@ int _printf(const char *format, ...)
 	unsigned int i;
 	int typeChecked = 0, charReturned = 0;
 	va_list arg;
-/* char *format(va_list); */
 
 	va_start(arg, format);
-
-/* if (format == NULL) */
-/* { */
-/* return (-1); */
-/* } */
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-		{
 			_putchar(format[i]);
 			charReturned++;
 			continue;
-		}
 
 		if (format[i + 1] == '%')
-		{
 			_putchar('%');
 			charReturned++;
 			i++;
 			continue;
-		}
 
 		if (format[i + 1] == '\0')
-		{
 			return (-1);
-		}
 
 		typeChecked = printTypeChecker(format[i + 1], arg);
-		
+
 		if (typeChecked == -1 || typeChecked != 0)
-		{
 			i++;
-		}
 
 		if (typeChecked > 0)
-		{
 			charReturned += typeChecked;
-		}
 
 		if (typeChecked == 0)
-		{
 			_putchar('%');
 			charReturned++;
-		}
 
 	}
 
