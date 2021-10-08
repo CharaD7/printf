fe6ffdb9ce0642ea034bbfd0c8b6741e99c28bc2
CharaD7 <jijakahn6@gmail.com>
2021-10-08 23:19:07 +0000

    Took care of linting errors

--- printf.c
+++ printf.c
@@ -46,40 +46,32 @@ int _printf(const char *format, ...)
 	unsigned int i;
 	int typeChecked = 0, charReturned = 0;
 	va_list arg;
+
 	va_start(arg, format);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-		{
 			_putchar(format[i]);
 			charReturned++;
 			continue;
-		}
 		if (format[i + 1] == '%')
-		{
 			_putchar('%');
 			charReturned++;
 			i++;
 			continue;
-		}
 		if (format[i + 1] == '\0')
-		{
 			return (-1);
-		}
+
 		typeChecked = printTypeChecker(format[i + 1], arg);
+
 		if (typeChecked == -1 || typeChecked != 0)
-		{
 			i++;
-		}
 		if (typeChecked > 0)
-		{
 			charReturned += typeChecked;
-		}
 		if (typeChecked == 0)
-		{
 			_putchar('%');
 			charReturned++;
-		}
 	}
 	va_end(arg);
 	return (charReturned);
