2895a3b4a3e0b58e9c6bbe05ddde19dc7595572a
CharaD7 <jijakahn6@gmail.com>
2021-10-09 00:06:57 +0000

    Took care of linting errors

--- main.h
+++ main.h
@@ -9,14 +9,14 @@ int ret_str(va_list arg);
 int _putchar(char c);
 int ret_int(va_list arg);
 int ret_percent(void);
-void ret_binary(unsigned int n, unsigned int* printed);
+void ret_binary(unsigned int n, unsigned int *printed);
 int ret_unsignedToBinary(va_list arg);
 int ret_oct(va_list arg);
 int ret_unsignedIntToHex(unsigned int num, char _case);
 int ret_hex_base(va_list arg, char _case);
 int ret_hex(va_list arg);
 int ret_HEX(va_list arg);
-int ret_STR (va_list arg);
+int ret_STR(va_list arg);
 int ret_unsigned(va_list arg);
 
 /**
--- ret_func.c
+++ ret_func.c
@@ -114,35 +114,6 @@ int ret_STR(va_list arg)
 	return (i);
 }
 
-/**
- * ret_str - prints a string with a `s` (lower case) specifier
- * @arg: argument
- * Return: number of character printed
- */
-
-int ret_str(va_list arg)
-{
-	int i;
-	char *str = va_arg(arg, char*);
-
-	if (str == NULL)
-		str = "(null)";
-	else if (*str == '\0')
-		return (-1);
-
-	for (i = 0; str[i]; i++)
-		_putchar(str[i]);
-
-	return (i);
-}
-
-/**
- * ret_unsigned - prints an unsigned int.
- * @arg: argument
- * Return: 0
- */
-
-int ret_unsigned(va_list arg)
 {
 	int divisor = 1, i, resp;
 	unsigned int n = va_arg(arg, unsigned int);
